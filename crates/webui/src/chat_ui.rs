c5fa2c71ccd46807e66de2464ba2305775a4d126
organicoverlords <finaldope@gmail.com>
2026-07-01 02:53:57 +0300

    Wire human action summaries into WebUI

--- crates/webui/src/chat_ui.rs
+++ crates/webui/src/chat_ui.rs
@@ -9,6 +9,7 @@
 pub const CHAT_HTML: &str = concat!(
     include_str!("chat_ui.html"),
     include_str!("chat_ui_enhancements.html"),
+    include_str!("chat_ui_action_summaries.html"),
     include_str!("chat_ui_tool_lifecycle.html"),
     include_str!("chat_ui_session_controls.html"),
 );
