fabf1f3f44d1143b742f162f1f8752f54b324f4e
organicoverlords <finaldope@gmail.com>
2026-07-01 09:52:23 +0300

    Bundle action digest pin controls

--- crates/webui/src/chat_ui.rs
+++ crates/webui/src/chat_ui.rs
@@ -10,6 +10,7 @@ pub const CHAT_HTML: &str = concat!(
     include_str!("chat_ui.html"),
     include_str!("chat_ui_enhancements.html"),
     include_str!("chat_ui_action_summaries.html"),
+    include_str!("chat_ui_action_pins.html"),
     include_str!("chat_ui_tool_lifecycle.html"),
     include_str!("chat_ui_session_controls.html"),
     include_str!("chat_ui_session_control_search.html"),
